ca3a0b76a11914e4552263187f58bbe80b84f187
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-10-21 12:15:49 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        cv: resolve(__dirname, "src/cv.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
